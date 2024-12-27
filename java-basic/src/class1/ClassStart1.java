b3eb6ec4e2c8fa6cc90853bdd1a82a7d4b6857b4
wongakim-99 <202021000@sangmyung.kr>
2024-12-27 11:28:31 +0900

    Feat : 간단한 변수를 선언하고 학생의 정보 출력
    Related to : #2

---
+++ java-basic/src/class1/ClassStart1.java
@@ -0,0 +1,21 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String studentName1 = "허준";
+        int studentAge1 = 26;
+        int studentGrade1 = 90;
+
+        String studentName2 = "김영웅";
+        int studentAge2 = 26;
+        int studentGrade2 = 20;
+
+        String studentName3 = "김가원";
+        int studentAge3 = 26;
+        int studentGrade3 = 56;
+
+        System.out.println("이름 : " + studentName1 + " 나이 : " + studentAge1 + " 성적 : " + studentGrade1);
+        System.out.println("이름 : " + studentName2 + " 나이 : " + studentAge2 + " 성적 : " + studentGrade2);
+        System.out.println("이름 : " + studentName3 + " 나이 : " + studentAge3 + " 성적 : " + studentGrade3);
+    }
+}

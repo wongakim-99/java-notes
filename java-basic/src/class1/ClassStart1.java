45351f1394778440afc553566b164a13afdc89e6
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-01-02 18:18:58 +0900

    Feat : "맥북 세팅후 테스트 커밋"
    Related to : #2

--- java-basic/src/class1/ClassStart1.java
+++ java-basic/src/class1/ClassStart1.java
@@ -14,8 +14,13 @@ public class ClassStart1 {
         int studentAge3 = 26;
         int studentGrade3 = 56;
 
+        String studentName4 = "황근출";
+        int studentAge4 = 26;
+        int studentGrade4 = 80;
+
         System.out.println("이름 : " + studentName1 + " 나이 : " + studentAge1 + " 성적 : " + studentGrade1);
         System.out.println("이름 : " + studentName2 + " 나이 : " + studentAge2 + " 성적 : " + studentGrade2);
         System.out.println("이름 : " + studentName3 + " 나이 : " + studentAge3 + " 성적 : " + studentGrade3);
+        System.out.println("이름 : " + studentName4 + " 나이 : " + studentAge4 + " 성적 : " + studentGrade4);
     }
 }

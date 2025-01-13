941c2eb66881de6dedb55b6c48c3a651ed50df6c
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-01-13 11:12:36 +0900

    [#3] null

--- java-basic/src/ref/NullMain1.java
+++ java-basic/src/ref/NullMain1.java
@@ -3,9 +3,11 @@ package ref;
 public class NullMain1 {
     public static void main(String[] args) {
         Data data = null;
-        System.out.println("1. data = " + data);
+        System.out.println("1. data = " + data);  // data = null
+
         data = new Data();
-        System.out.println("2. data = " + data);
+        System.out.println("2. data = " + data);  // 새로운 객체를 생성했으니 가리키는 참조값이 있을 것이다.
+
         data = null;
         System.out.println("3. data = " + data);
     }

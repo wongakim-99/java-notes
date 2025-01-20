eb8d5f1fe30f3ec209a2934d5593298ddf17c14f
김가원(Kimgawon) <kgw1999zz@naver.com>
2025-01-20 14:06:35 +0900

    [#5] this와 생성자

--- java-basic/src/constructor/MethodInitMain3.java
+++ java-basic/src/constructor/MethodInitMain3.java
@@ -3,10 +3,10 @@ package constructor;
 public class MethodInitMain3 {
     public static void main(String[] args) {
         MemberInit member1 = new MemberInit();
-        //member1.initMember("user1", 15, 90);
+        member1.initMember("user1", 15, 90);
 
         MemberInit member2 = new MemberInit();
-        // member2.initMember("user2", 15, 90);
+        member2.initMember("user2", 15, 90);
 
         MemberInit[] members = {member1, member2};
 

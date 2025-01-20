c4c9b30464d38920e12ee500776f389de500041b
김가원(Kimgawon) <kgw1999zz@naver.com>
2025-01-20 14:41:30 +0900

    [#5] 생성자 도입과 장점

--- java-basic/src/constructor/ConstructMain1.java
+++ java-basic/src/constructor/ConstructMain1.java
@@ -2,13 +2,12 @@ package constructor;
 
 public class ConstructMain1 {
     public static void main(String[] args) {
-        // 객체 생성과 동시에 생성자 호출
-        MemberConstructor member1 = new MemberConstructor("김영웅", 26, 80);
-        MemberConstructor member2 = new MemberConstructor("김가원", 36, 90);
+        MemberConstructor member1 = new MemberConstructor("김영웅", 19, 90);
+        MemberConstructor member2 = new MemberConstructor("자르반 4세", 55, 90);
 
         MemberConstructor[] members = {member1, member2};
 
-        for (MemberConstructor s: members) {
+        for (MemberConstructor s : members) {
             System.out.println("이름:" + s.name + " 나이:" + s.age + " 성적:" + s.grade);
         }
     }
--- java-basic/src/constructor/MemberConstructor.java
+++ java-basic/src/constructor/MemberConstructor.java
@@ -5,12 +5,8 @@ public class MemberConstructor {
     int age;
     int grade;
 
-    MemberConstructor(String name, int age) {
-        this(name, age, 50);
-    }
-
     MemberConstructor(String name, int age, int grade) {
-        System.out.println("생성자 호출 name = " + name + ", age = " + age + ", grade = " + grade);
+        System.out.println("생성자 호출 name=" + name + ",age=" + age + ",grade=" + grade);
         this.name = name;
         this.age = age;
         this.grade = grade;

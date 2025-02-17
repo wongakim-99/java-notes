f8ad778b13e54e037704af017d616c8329f635de
김가원(Kimgawon) <kgw1999zz@naver.com>
2025-02-17 23:23:44 +0900

    [#7] 접근 제어자 마무리

--- java-basic/src/access/b/PublicClassOuterMain.java
+++ java-basic/src/access/b/PublicClassOuterMain.java
@@ -1,7 +1,5 @@
 package access.b;
 
-import access.a.DefaultClass1;
-import access.a.DefaultClass2;
 import access.a.PublicClass;
 
 public class PublicClassOuterMain {
@@ -9,7 +7,7 @@ public class PublicClassOuterMain {
         PublicClass publicClass = new PublicClass();
 
         // 다른 패키지로 접근 불가
-        DefaultClass1 class1 = new DefaultClass1();
-        DefaultClass2 class2 = new DefaultClass2();
+        // DefaultClass1 class1 = new DefaultClass1();
+        // DefaultClass2 class2 = new DefaultClass2();
     }
 }

ee26a637e10bed9620dcb08fd092f919a96eb8d2
김가원(Kimgawon) <kgw1999zz@naver.com>
2025-02-21 17:22:06 +0900

    [#8] static 메서드1

---
+++ java-basic/src/static2/DecoMain1.java
@@ -0,0 +1,13 @@
+package static2;
+
+public class DecoMain1 {
+    public static void main(String[] args) {
+        String s = "Hello";
+        DecoUtil utils = new DecoUtil();
+
+        String deco = utils.deco(s);
+
+        System.out.println("before : " + s);
+        System.out.println("after : " + deco);
+    }
+}
---
+++ java-basic/src/static2/DecoUtil.java
@@ -0,0 +1,9 @@
+package static2;
+
+public class DecoUtil {
+
+    public String deco(String str) {
+        String result = "*" + str + "*";
+        return result;
+    }
+}

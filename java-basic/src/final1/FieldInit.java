68d0a6a0acf18800cf6ce7de8114d59c996dad32
김가원(Kimgawon) <kgw1999zz@naver.com>
2025-02-22 18:33:47 +0900

    [#9] final 변수 복습1

--- java-basic/src/final1/FieldInit.java
+++ java-basic/src/final1/FieldInit.java
@@ -2,10 +2,6 @@ package final1;
 
 public class FieldInit {
 
-    static final int CONST_VALUE = 10;
+    static final int CONST_VALUE = 40;
     final int value = 10;
-
-    public FieldInit() {
-        // this.value = value;  // 컴파일 에러! -> 파이널은 값이 한번 세팅되면 그걸로 끝
-    }
 }
--- java-basic/src/final1/FinalFieldMain.java
+++ java-basic/src/final1/FinalFieldMain.java
@@ -2,24 +2,28 @@ package final1;
 
 public class FinalFieldMain {
     public static void main(String[] args) {
-        // final 필드
+        // final 필드 - 생성자 초기화
         System.out.println("생성자 초기화");
         ConstructorInit constructorInit1 = new ConstructorInit(10);
         ConstructorInit constructorInit2 = new ConstructorInit(20);
+        ConstructorInit constructorInit3 = new ConstructorInit(10);
+
         System.out.println(constructorInit1.value);
         System.out.println(constructorInit2.value);
+        System.out.println(constructorInit3.value);
 
         // final 필드 - 필드 초기화
         System.out.println("필드 초기화");
         FieldInit fieldInit1 = new FieldInit();
         FieldInit fieldInit2 = new FieldInit();
         FieldInit fieldInit3 = new FieldInit();
+
         System.out.println(fieldInit1.value);
         System.out.println(fieldInit2.value);
         System.out.println(fieldInit3.value);
 
         // 상수
         System.out.println("상수");
-        System.out.println(FieldInit.CONST_VALUE);
+        System.out.println(fieldInit1.CONST_VALUE);
     }
 }

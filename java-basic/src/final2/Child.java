d9323b661d6588158c6b8cb941ca232ef334abe3
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-02-23 16:25:55 +0900

    [#9] final 관련 오버라이딩 예시

---
+++ java-basic/src/final2/Child.java
@@ -0,0 +1,7 @@
+package final2;
+
+public class Child extends Parent {
+//    void show() {  // 컴파일 에러 (final 메서드는 오버라이딩 불가)
+//        System.out.println("Child show");
+//    }
+}
---
+++ java-basic/src/final2/Parent.java
@@ -0,0 +1,7 @@
+package final2;
+
+public class Parent {
+    final void show() {
+        System.out.println("Parent 클래스의 show() 메서드");
+    }
+}
